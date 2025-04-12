f8c802492197aeb94819ba5de636980b801c22e5
Genesis <dmsgktn1685@naver.com>
2025-04-12 18:14:58 +0900

    [Feat] #3 - Resource 모듈의 자원을 쉽게 사용할 수 있도록 UIColor& UIFont & UIImage Extension 추가

---
+++ Terbuck/DesignSystem/Sources/Color/TerbuckColor.swift
@@ -0,0 +1,22 @@
+//
+//  TerbuckColor.swift
+//  DesignSystem
+//
+//  Created by ParkJunHyuk on 4/11/25.
+//
+
+import UIKit
+import Resource
+
+public extension UIColor {
+    static let terbuckBlack5 = UIColor(asset: ResourceAsset.Color.terbuckBlack5)!
+    static let terbuckBlack10 = UIColor(asset: ResourceAsset.Color.terbuckBlack10)!
+    static let terbuckBlack30 = UIColor(asset: ResourceAsset.Color.terbuckBlack30)!
+    static let terbuckBlack50 = UIColor(asset: ResourceAsset.Color.terbuckBlack50)!
+    static let terbuckDarkGray10 = UIColor(asset: ResourceAsset.Color.terbuckDarkGray10)!
+    static let terbuckDarkGray50 = UIColor(asset: ResourceAsset.Color.terbuckDarkGray50)!
+    static let terbuckGreen10 = UIColor(asset: ResourceAsset.Color.terbuckGreen10)!
+    static let terbuckGreen50 = UIColor(asset: ResourceAsset.Color.terbuckGreen50)!
+    static let terbuckWhite = UIColor(asset: ResourceAsset.Color.terbuckWhite)!
+    static let terbuckWhite5 = UIColor(asset: ResourceAsset.Color.terbuckWhite5)!
+}
---
+++ Terbuck/DesignSystem/Sources/Image/TerbuckImage.swift
@@ -0,0 +1,27 @@
+//
+//  TerbuckImage.swift
+//  DesignSystem
+//
+//  Created by ParkJunHyuk on 4/11/25.
+//
+
+import UIKit
+import Resource
+
+public extension UIImage {
+    static var kakaoLogo: UIImage {
+        return ResourceAsset.Image.kakaoLogo.image
+    }
+    
+    static var appleLogo: UIImage {
+        return ResourceAsset.Image.appleLogo.image
+    }
+    
+    static var notSelectedCheck: UIImage {
+        return ResourceAsset.Image.notSelectedCheck.image
+    }
+    
+    static var selectedCheck: UIImage {
+        return ResourceAsset.Image.selectedCheck.image
+    }
+}
