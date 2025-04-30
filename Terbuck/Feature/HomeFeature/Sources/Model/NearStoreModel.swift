cc2ffdeb5dd6ccc74bdec64f8e169504aa50adf7
Genesis <dmsgktn1685@naver.com>
2025-04-30 21:11:32 +0900

    [Fix] #15 - Model 의 UUID 관련 로직 수정

--- Terbuck/Feature/HomeFeature/Sources/Model/NearStoreModel.swift
+++ Terbuck/Feature/HomeFeature/Sources/Model/NearStoreModel.swift
@@ -8,26 +8,14 @@
 import Foundation
 import Shared
 
-public struct NearStoreModel: StoreItemIdentifiable, Hashable {
-    var id: String {
+public struct NearStoreModel: Identifiable, Hashable {
+    public var id: String {
         return "\(storeName)-\(address)"
     }
-    
+
     var storeName: String
     var cateotry: CategoryType
     var address: String
     var mainBenefit: String
     var subBenefit: [String]? = nil
-    
-    public func hash(into hasher: inout Hasher) {
-      hasher.combine(id)
-    }
-    
-    public static func == (lhs: Self, rhs: Self) -> Bool {
-      lhs.id == rhs.id
-    }
-}
-
-protocol StoreItemIdentifiable: Hashable {
-    var id: String { get } // 고유 식별자
 }
--- Terbuck/Feature/HomeFeature/Sources/Model/PartnershipModel.swift
+++ Terbuck/Feature/HomeFeature/Sources/Model/PartnershipModel.swift
@@ -8,21 +8,13 @@
 import Foundation
 import Shared
 
-public struct PartnershipModel: StoreItemIdentifiable, Hashable {
-    var id: String {
-        return "\(partnershipName)-\(partnerCategoryType)"
+public struct PartnershipModel: Identifiable, Hashable {
+    public var id: String {
+        return "\(partnershipName) - \(partnerCategoryType)"
     }
     
     var partnershipName: String
     var partnerCategoryType: PartnerCategoryType
     var storeType: PartnerStoreType
     var isNewPartner: Bool
-    
-    public func hash(into hasher: inout Hasher) {
-      hasher.combine(id)
-    }
-    
-    public static func == (lhs: Self, rhs: Self) -> Bool {
-      lhs.id == rhs.id
-    }
 }
