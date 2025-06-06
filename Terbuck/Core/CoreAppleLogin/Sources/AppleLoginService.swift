2fa2dd0e68c4e148f1828d8a18a81c0beeb02aa4
Genesis <dmsgktn1685@naver.com>
2025-06-06 14:08:53 +0900

    [Feat] #21 - 각 소셜 로그인 관련 CoreApple, CoreKakao Login 모듈 구현

---
+++ Terbuck/Core/CoreAppleLogin/Sources/AppleLoginService.swift
@@ -0,0 +1,61 @@
+//
+//  AppleLoginService.swift
+//  Core
+//
+//  Created by ParkJunHyuk on 5/25/25.
+//
+
+import AuthenticationServices
+
+public final class AppleLoginService: NSObject {
+    public static let shared = AppleLoginService()
+    
+    private override init() {}
+    
+    private var continuation: CheckedContinuation<(code: String, name: String), Error>?
+
+   public func login() async throws -> (code: String, name: String) {
+       return try await withCheckedThrowingContinuation { continuation in
+           self.continuation = continuation
+           let provider = ASAuthorizationAppleIDProvider()
+           let request = provider.createRequest()
+           request.requestedScopes = [.fullName]
+           let controller = ASAuthorizationController(authorizationRequests: [request])
+           controller.delegate = self
+           controller.presentationContextProvider = self
+           controller.performRequests()
+       }
+   }
+}
+
+extension AppleLoginService: ASAuthorizationControllerDelegate {
+    public func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        
+        guard let credential = authorization.credential as? ASAuthorizationAppleIDCredential else {
+            continuation?.resume(throwing: AppleLoginError.noCredential)
+            return
+        }
+        
+        let userName = credential.fullName?.givenName ?? "Unknown"
+        let authCode = String(data: credential.authorizationCode ?? Data(), encoding: .utf8) ?? ""
+        
+        print("애플로그인 결과 반환",authCode, userName)
+        
+        let responseDTO = AppleLoginResponseDTO(
+            authCode: authCode,
+            userName: userName
+        )
+        
+        continuation?.resume(returning: (code: authCode, name: userName))
+   }
+
+   public func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+       continuation?.resume(throwing: error)
+   }
+}
+
+extension AppleLoginService: ASAuthorizationControllerPresentationContextProviding {
+   public func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+       UIApplication.shared.windows.first { $0.isKeyWindow } ?? UIWindow()
+   }
+}
