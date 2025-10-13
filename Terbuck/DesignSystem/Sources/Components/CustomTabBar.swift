c0f9330193eb41d41db1db3af520d51b389b5ac1
Genesis <dmsgktn1685@naver.com>
2025-10-13 16:58:59 +0900

    [Fix] #82 - 코드리뷰 반영

--- Terbuck/DesignSystem/Sources/Components/CustomTabBar.swift
+++ Terbuck/DesignSystem/Sources/Components/CustomTabBar.swift
@@ -85,7 +85,7 @@ private extension CustomTabBar {
     
     func setupHierarchy() {
         addSubviews(topSeparator, backgroundView)
-        backgroundView.addSubview(stackView)
+        backgroundView.addSubviews(stackView)
     }
     
     func setupLayout() {
